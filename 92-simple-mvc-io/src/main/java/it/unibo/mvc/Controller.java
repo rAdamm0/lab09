babb88aaa21c8fe6cebe2586b570e47a6274a2ad
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2024-11-20 10:41:26 +0100

    created the interface in which to text and the save button

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,11 +1,8 @@
 package it.unibo.mvc;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintStream;
 
-import javax.swing.JOptionPane;
 
 /**
  * Application controller. Performs the I/O.
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,20 +1,49 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUI {
-
-    private final JFrame frame = new JFrame();
+    private final String TITLE = "My first graphical interface";
+    private final JFrame frame = new JFrame(TITLE);
+    private final int PROPORTION = 3;
+    
+    public SimpleGUI(){
+        JPanel canva = new JPanel(new BorderLayout());
+        JButton save = new JButton("Save");
+        JTextArea text = new JTextArea();
+        frame.setContentPane(canva);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        canva.add(save,BorderLayout.SOUTH);
+        canva.add(text);
+    }
+    
 
     private void display(){
+        final Dimension ss =  Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int)ss.getWidth();
+        final int sh = (int)ss.getHeight();
+        frame.setSize(sw/this.PROPORTION, sh/this.PROPORTION);
+        //frame.pack();
+
+        frame.setLocationByPlatform(true);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... args){
+        new SimpleGUI().display();
     }
 }
 
 
-public static void main(final String... args){
-    new SimpleGUI().display();
-}
