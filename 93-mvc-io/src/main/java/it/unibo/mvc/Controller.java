c2341c44d3b0e49449218913e9ee449ea9803226
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2024-11-20 15:41:03 +0100

    Created a first prompt of the Controller Interface and the first part of the setNextString method

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,18 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+
 /**
  *
  */
 public interface Controller {
+
+    public String setNextString(String input);
+
+    public String nexString();
+
+    public ArrayList<String> history();
+
+    public void currentString();
 
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,60 @@
 package it.unibo.mvc;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.PrintStream;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private String current;
+
+    private String nameFile;
+    private String PATH = System.getProperty("user.home")+System.getProperty("file.separator")+nameFile;
+
+    private void updatePath(){
+        PATH = System.getProperty("user.home")+System.getProperty("file.separator")+nameFile;
+    }
+
+    public String setNameFile(String input){
+        return this.nameFile = input;
+    }
+
+    public String getPath(){
+        updatePath();
+        return this.PATH;
+    }
+
+    @Override
+    public void setNextString(String input) {
+        try(PrintStream ps = new PrintStream(this.PATH, StandardCharsets.UTF_8)) {
+            ps.append(input);
+        } catch (IOException e) {
+            e.getStackTrace();
+            System.err.println("ERROR in setNextString");
+        }
+    }
+
+    @Override
+    public String nexString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nexString'");
+    }
+
+    @Override
+    public ArrayList<String> history() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'history'");
+    }
+
+    @Override
+    public void currentString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'currentString'");
+    }
 
 }
