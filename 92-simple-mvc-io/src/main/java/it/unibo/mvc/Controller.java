109ea1c792fda9acd9ca935b52ac4bf4bb3690d7
Adam Paolo Razzino <adampaolo.razzino@studio.unibo.it>
2025-11-18 11:21:24 +0100

    Implemented the controller in exercise 92

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,40 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    private final String USER_HOME = System.getProperty("user.home");
+    private final String SEPARATOR = System.getProperty("file.separator");
+    private File file = new File(USER_HOME + SEPARATOR + "output.txt");
+
+    public void setFile(String fileName) {
+        this.file = new File(USER_HOME + SEPARATOR + fileName);
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return this.file;
+    }
+
+    public String getPath() {
+        return this.file.getAbsolutePath();
+    }
+
+    public void writeToFile(String messaage) {
+        try (PrintStream ps = new PrintStream(file)) {
+            ps.println(messaage);
+        } catch (final IOException e) {
+            System.out.println("The writeToFile didnt complete, here is the error: " + e.getMessage());
+        }
+    }
 
 }
