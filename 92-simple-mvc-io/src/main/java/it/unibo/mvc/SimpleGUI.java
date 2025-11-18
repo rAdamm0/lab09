1ffa4ba8aa2f47d94d2104c3e6637469d42c7338
Adam Paolo Razzino <adampaolo.razzino@studio.unibo.it>
2025-11-18 11:46:05 +0100

    Implemented the Simple GUI in exercise 92 to write to file the content of the JTextArea in the user.home/output.txt file

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,13 +1,71 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Closeable;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUI {
+    /* Global fields creation */
+    private final Controller control = new Controller();
+
+    private final int PROPORTION = 5;
 
     private final JFrame frame = new JFrame();
+
+    public SimpleGUI() {
+        /* JFrame finalization */
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /* JPanel Creation */
+        JPanel canva = new JPanel();
+        frame.setContentPane(canva);
+        canva.setLayout(new BorderLayout());
+
+        /* JTextArea and Button Creation */
+        JTextArea textArea = new JTextArea();
+        JButton saveBtn = new JButton("Save");
+        canva.add(textArea);
+        canva.add(saveBtn, BorderLayout.SOUTH);
+
+        /* Implementation of the JButton */
+        saveBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent ignored) {
+                saveBtn.setEnabled(false);
+                control.writeToFile(textArea.getText());
+                System.out.println("Text written succesfully");
+                saveBtn.setEnabled(true);
+            }
+        });
+
+    }
+
+    public void display() {
+        /* Makes the displayed window to a 5th of the Screen */
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screenSize.getWidth() / PROPORTION;
+        final int sh = (int) screenSize.getHeight() / PROPORTION;
+        frame.setSize(sw, sh);
+
+        frame.setLocationByPlatform(true);
+
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI().display();
+    }
 
 }
